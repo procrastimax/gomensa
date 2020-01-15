c3e7eb03437493ae0c182292fb772799cbb4aee8
Procrastimax <heykeroth.dev@gmail.com>
2020-01-15 19:19:29 +0100

    Improves function for requesting all canteens
    The RequestListOfAllCanteens now uses channels to retrieve all available
    canteens and then appends them a allCanteens slice which is returned by
    the function.
    
    Also updating the test for this function.

--- request_test.go
+++ request_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRequestAllCanteens(t *testing.T) {
-	canteens := requestCanteens(1)
+	canteens := RequestListOfAllCanteens()
 
 	if len(canteens) == 0 {
 		t.Error("Did not retrieve any canteens when requesting a list of them!")
+	} else {
+		fmt.Println(canteens)
+		fmt.Println("canteen count:", len(canteens))
 	}
 }
--- requester.go
+++ requester.go
@@ -21,28 +21,31 @@ type Canteen struct {
 
 //RequestListOfAllCanteens request all canteens from all api pages and return a list of all
 func RequestListOfAllCanteens() []Canteen {
-	allCanteens := make([]Canteen, 100)
+	//currently there are more than 496 canteens, so we can allocate some memory before appending the slices -> Im not gonna update this value in the future
+	allCanteens := make([]Canteen, 0, 496)
 
+	canteensChan := make(chan []Canteen)
 	page := 1
-	for page <= 10 {
-		requestCanteens(page)
+	for {
+		go requestCanteens(page, canteensChan)
 		page++
-	}
 
+		value, ok := <-canteensChan
+		//request and parse responses until the channel is closed -> this happens when we reach the max page number
+		if ok == false {
+			break
+		}
+		allCanteens = append(allCanteens, value...)
+	}
 	return allCanteens
 }
 
 //requestCanteens makes a GET request to the openmensa endpoint and returns a list of all canteens
-func requestCanteens(page int) []Canteen {
-	if page <= 0 {
-		log.Println("ERROR: Cannot use page parameter less than 1!")
-		return []Canteen{}
-	}
-
+func requestCanteens(page int, canteensChan chan<- []Canteen) {
 	baseURL, err := url.Parse(openMensaEndpoint)
 	if err != nil {
 		log.Println("ERROR: Malformed URL ", err.Error())
-		return []Canteen{}
+		close(canteensChan)
 	}
 
 	// Add a Path Segment (Path segment is automatically escaped)
@@ -56,28 +59,36 @@ func requestCanteens(page int) []Canteen {
 	baseURL.RawQuery = params.Encode()
 
 	resp, err := http.Get(baseURL.String())
-
 	if err != nil {
 		log.Println("ERROR: Something went wrong when requesting a list of all canteens!", err.Error())
-		return []Canteen{}
+		close(canteensChan)
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
 		log.Println("ERROR: Something went wrong when processing the result of requesting a list of all canteens!", err.Error())
-		return []Canteen{}
+		close(canteensChan)
 	}
 
 	canteens := make([]Canteen, 0)
 	err = json.Unmarshal(body, &canteens)
-
 	if err != nil {
 		log.Println("ERROR: Something went wrong when trying to parse the canteen list results!", err.Error())
-		return []Canteen{}
+		close(canteensChan)
 	}
 
-	return canteens
+	canteensChan <- canteens
+
+	// check if the next page would be the last page and then closes the channel
+	maxPages, err := strconv.Atoi(resp.Header.Get("X-Total-Pages"))
+	if err != nil {
+		log.Println("ERROR: Could not convert max page header key to int!")
+		close(canteensChan)
+	}
+
+	if page+1 > maxPages {
+		close(canteensChan)
+	}
 }
